2f9b2cb59b9810eabbafb32059541e5cd755d428
Pablo Arteaga <dj.pablo.ac@gmail.com>
2025-06-11 22:51:08 -0500

    Add authentication middleware and update category routes to use it; create docker-compose file for SQL Server

--- backend/src/categories/category.routes.js
+++ backend/src/categories/category.routes.js
@@ -1,14 +1,15 @@
 import express from 'express';
 
 import * as categoryController from './category.controller.js';
+import { authBearer } from '../middlewares/auth.js';
 
 const router = express.Router();
 
-router.get('/', categoryController.getCategories.bind(categoryController));
-router.get('/:id', categoryController.getCategoryById.bind(categoryController));
-router.post('/', categoryController.createdCategory.bind(categoryController));
-router.put('/:id', categoryController.updateCategory.bind(categoryController));
-router.delete('/:id', categoryController.deleteCategory.bind(categoryController));
+router.get('/', authBearer, categoryController.getCategories.bind(categoryController));
+router.get('/:id', authBearer, categoryController.getCategoryById.bind(categoryController));
+router.post('/', authBearer, categoryController.createdCategory.bind(categoryController));
+router.put('/:id', authBearer, categoryController.updateCategory.bind(categoryController));
+router.delete('/:id', authBearer, categoryController.deleteCategory.bind(categoryController));
 
 export default router;
 
